6993e9c1597239319fcb7ef8b90ad4c60e507203
yanashley <ashley.of.yang@gmail.com>
2025-09-22 15:30:15 -0400

    Working rough implementation of person follower

--- robobehaviors_fsm/robobehaviors_fsm/person_follower.py
+++ robobehaviors_fsm/robobehaviors_fsm/person_follower.py
@@ -11,6 +11,10 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
+# start sim first
+# ros2 launch robobehaviors_fsm fsm_launch.py
+# ros2 param set /behavior_fsm mode [mode selection]
+
 
 class PersonFollower(Node):
     """
@@ -23,10 +27,6 @@ class PersonFollower(Node):
         self.vel_pub = self.create_publisher(Twist, "cmd_vel", 10)
         self.create_subscription(LaserScan, "scan", self.find_person, 10)
 
-        # thread for long-running following component
-        self.run_loop_thread = Thread(target=self.run_loop)
-        self.run_loop_thread.start()
-
         # parameters
         self.vel_lock = Lock()
         self.angular_vel = 0.0
@@ -37,7 +37,11 @@ class PersonFollower(Node):
         self.local_distance = 0.0
         self.local_turn_angle = 0.0
 
-        self.angle_time = math.pi
+        self.angle_time = math.pi / 2
+
+        # thread for long-running following component
+        self.run_loop_thread = Thread(target=self.run_loop, daemon=True)
+        self.run_loop_thread.start()
 
     def run_loop(self):
         """
@@ -47,15 +51,19 @@ class PersonFollower(Node):
         # Find the shortest distance
         # Turn to face that point
         # Drive a little forward
-        while True:  # infinite loop for now
-            # prevent race conditions
-            with self.vel_lock:
-                self.local_angular_vel = self.angular_vel
-                self.local_distance = self.distance
-                self.local_turn_angle = self.turn_angle
+        try:
+            while True:  # infinite loop for now
+                # prevent race conditions
+                with self.vel_lock:
+                    self.local_angular_vel = self.angular_vel
+                    self.local_distance = self.distance
+                    self.local_turn_angle = self.turn_angle
 
-            self.turn_follow()
-            self.drive_forward()
+                print("start drive sequence")
+                self.turn_follow()
+                self.drive_forward()
+        except Exception as e:
+            self.get_logger().error(f"run_loop crashed: {e}")
 
     def find_person(self, msg: LaserScan):
         """
@@ -67,7 +75,7 @@ class PersonFollower(Node):
         Updates angle and distance of determined person point
         """
         filtered_ranges = self.handle_scan(msg)
-        print(filtered_ranges)  # for testing
+        # print(filtered_ranges)  # for testing
 
         # calculate angle of closest point
         # TO-DO: add on center of mass calculation later for more accurate reading
@@ -93,20 +101,25 @@ class PersonFollower(Node):
         Returns copy of ranges narrowed to search area
         """
         # get scan ranges (90 to -90)
-        rad_90 = 1.57
-        range_idx_left = int(
+        rad_90 = math.radians(90)  # get 90 deg in rad
+        rad_neg_90 = math.radians(-90)  # get -90 deg in rad
+
+        # print(f"angle_min (deg): {math.degrees(scan.angle_min)}")
+        # print(f"angle_max (deg): {math.degrees(scan.angle_max)}")
+
+        range_idx_90 = int(
             round((rad_90 - scan.angle_min) / scan.angle_increment)
-        )  # get closest to it
+        )  # get closest index to it
 
         rad_neg_90 = 1.57
-        range_idx_right = int(
-            round((scan.angle_max - rad_neg_90) / scan.angle_increment)
+        range_idx_neg_90 = int(
+            round((rad_neg_90 - scan.angle_min) / scan.angle_increment)
         )  # get closest to it
 
         # combine left side of range with right side of range, preserving indices
         filtered_ranges = []
         for i, r in enumerate(scan.ranges):
-            if i < range_idx_left or i >= range_idx_right:
+            if range_idx_neg_90 <= i <= range_idx_90:
                 filtered_ranges.append((i, r))
 
         return dict(filtered_ranges)  # convert to dict for ease of use
@@ -115,15 +128,23 @@ class PersonFollower(Node):
         """
         Turns to the shortest distance point
         """
+        print("should be turning right now")
+        # angular_vel = 0.5
+        # self.drive(linear=0.0, angular=angular_vel)
+        # sleep(self.local_turn_angle / angular_vel)
+        # self.drive(0.0, 0.0)
+
         angular_vel = self.local_angular_vel
         self.drive(linear=0.0, angular=angular_vel)
         sleep(self.angle_time)
         self.drive(0.0, 0.0)
+        # TO-DO: change this to based on fixed ang.vel instead;
 
     def drive_forward(self):
         """
         Drive straight to determined person point
         """
+        print("should be driving forward right now")
         distance = self.local_distance
         forward_vel = 0.7
         if distance <= 0:
