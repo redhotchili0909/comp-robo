bcf69c2a38f328e4d143ceed6204074976677cca
yanashley <ashley.of.yang@gmail.com>
2025-09-24 04:01:27 -0400

    Fix person follower object recognition
    Realized one range indice is one degree, simplified scan processing code,
    adjusted for negatives turning angles, adjusted distance recognition

--- robobehaviors_fsm/robobehaviors_fsm/person_follower.py
+++ robobehaviors_fsm/robobehaviors_fsm/person_follower.py
@@ -3,17 +3,14 @@ This node has the robot follow a person
 """
 
 from time import sleep
-from threading import Thread, Lock
+from threading import Thread, Lock, Event
 import math
 
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
-
-# start sim first
-# ros2 launch robobehaviors_fsm fsm_launch.py
-# ros2 param set /behavior_fsm mode [mode selection]
+from std_msgs.msg import String
 
 
 class PersonFollower(Node):
@@ -27,133 +24,99 @@ class PersonFollower(Node):
         self.vel_pub = self.create_publisher(Twist, "cmd_vel", 10)
         self.create_subscription(LaserScan, "scan", self.find_person, 10)
 
+        # subscribe to FSM node
+        # self._enabled = Event()
+        # self.create_subscription(String, "fsm/state", self._on_fsm_state, 10)
+
         # parameters
         self.vel_lock = Lock()
         self.angular_vel = 0.0
         self.distance = 0.0
         self.turn_angle = 0.0
 
-        self.local_angular_vel = 0.0
         self.local_distance = 0.0
         self.local_turn_angle = 0.0
 
         self.angle_time = math.pi / 2
 
+        self.is_person = False
+
         # thread for long-running following component
         self.run_loop_thread = Thread(target=self.run_loop, daemon=True)
         self.run_loop_thread.start()
+
+        self.get_logger().info("person follower initialized")
+
+    # def _on_fsm_state(self, msg: String):
+    #     if msg.data == "PERSON":
+    #         if not self._enabled.is_set():
+    #             self.get_logger().info("Activated by FSM (PERSON)")
+    #         self._enabled.set()
+    #     else:
+    #         if self._enabled.is_set():
+    #             self.get_logger().info("Deactivated by FSM (leaving PERSON)")
+    #         self._enabled.clear()
+    #         self.drive(0.0, 0.0)
 
     def run_loop(self):
         """
         Executes main person following logic
         """
-        # Detect sweep of laser scans within range of the front
-        # Find the shortest distance
-        # Turn to face that point
-        # Drive a little forward
         try:
-            while True:  # infinite loop for now
-                # prevent race conditions
-                with self.vel_lock:
-                    self.local_angular_vel = self.angular_vel
-                    self.local_distance = self.distance
-                    self.local_turn_angle = self.turn_angle
-
-                print("start drive sequence")
-                self.turn_follow()
-                self.drive_forward()
+            while rclpy.ok():
+                # if self._enabled.is_set():
+                #     if self.is_person:
+                #         # prevent race conditions
+                #         with self.vel_lock:
+                #             self.local_distance = self.distance
+                #             self.local_turn_angle = self.turn_angle
+                #         self.get_logger().debug("start drive sequence")
+                #         print("start drive sequence")
+                #         self.turn_follow()
+                #         self.drive_forward()
+                #     # don't need else is_person since drive should be 0.-
+                # else:
+                #         self.drive(linear=0.0, angular=0.0)
+                if self.is_person:
+                    # prevent race conditions
+                    with self.vel_lock:  # only update right before driving
+                        self.local_distance = self.distance
+                        self.local_turn_angle = self.turn_angle
+                    self.get_logger().info("start drive sequence")
+                    self.turn_follow()
+                    self.drive_forward()
+                    self.get_logger().info("end drive sequence")
+                # self.get_logger().info("ran loop")
         except Exception as e:
             self.get_logger().error(f"run_loop crashed: {e}")
-
-    def find_person(self, msg: LaserScan):
-        """
-        Checks the range of laser scans to find the shortest distance
-
-        Args:
-            msg (sensor_msgs.msg.LaserScan)
-
-        Updates angle and distance of determined person point
-        """
-        filtered_ranges = self.handle_scan(msg)
-        # print(filtered_ranges)  # for testing
-
-        # calculate angle of closest point
-        # TO-DO: add on center of mass calculation later for more accurate reading
-        min_angle_idx = min(
-            filtered_ranges, key=filtered_ranges.get
-        )  # sort based on distance, return angle idx
-
-        angle = msg.angle_min + (min_angle_idx * msg.angle_increment)
-        min_distance = min(filtered_ranges.values())
-
-        with self.vel_lock:
-            self.turn_angle = angle
-            self.angular_vel = angle / self.angle_time  # TO-DO update in a bit
-            self.distance = min_distance
-
-    def handle_scan(self, scan: LaserScan):
-        """
-        Handles and processes messages received on scan topic
-
-        Args:
-            msg (sensor_msgs.msg.LaserScan)
-
-        Returns copy of ranges narrowed to search area
-        """
-        # get scan ranges (90 to -90)
-        rad_90 = math.radians(90)  # get 90 deg in rad
-        rad_neg_90 = math.radians(-90)  # get -90 deg in rad
-
-        # print(f"angle_min (deg): {math.degrees(scan.angle_min)}")
-        # print(f"angle_max (deg): {math.degrees(scan.angle_max)}")
-
-        range_idx_90 = int(
-            round((rad_90 - scan.angle_min) / scan.angle_increment)
-        )  # get closest index to it
-
-        rad_neg_90 = 1.57
-        range_idx_neg_90 = int(
-            round((rad_neg_90 - scan.angle_min) / scan.angle_increment)
-        )  # get closest to it
-
-        # combine left side of range with right side of range, preserving indices
-        filtered_ranges = []
-        for i, r in enumerate(scan.ranges):
-            if range_idx_neg_90 <= i <= range_idx_90:
-                filtered_ranges.append((i, r))
-
-        return dict(filtered_ranges)  # convert to dict for ease of use
 
     def turn_follow(self):
         """
         Turns to the shortest distance point
         """
-        print("should be turning right now")
-        # angular_vel = 0.5
-        # self.drive(linear=0.0, angular=angular_vel)
-        # sleep(self.local_turn_angle / angular_vel)
-        # self.drive(0.0, 0.0)
-
-        angular_vel = self.local_angular_vel
-        self.drive(linear=0.0, angular=angular_vel)
+        self.get_logger().info(
+            f"should be turning {self.local_turn_angle:.2f} right now"
+        )
+        self.angular_vel = self.local_turn_angle / self.angle_time
+        self.drive(linear=0.0, angular=self.angular_vel)
         sleep(self.angle_time)
-        self.drive(0.0, 0.0)
-        # TO-DO: change this to based on fixed ang.vel instead;
+        self.drive(linear=0.0, angular=0.0)  # stop robot at end
+        self.get_logger().info(f"end turned {self.local_turn_angle:.2f}")
 
     def drive_forward(self):
         """
         Drive straight to determined person point
         """
-        print("should be driving forward right now")
         distance = self.local_distance
-        forward_vel = 0.7
+        self.get_logger().info(f"should be driving forward {distance:.2f} right now")
+        forward_vel = 0.1
         if distance <= 0:
             return
         duration = distance / forward_vel
-        # if not self._e_stop.is_set():
         self.drive(linear=forward_vel, angular=0.0)
-        sleep(duration)
-        self.drive(0.0, 0.0)
+        sleep(duration * 0.75)  # have robot go most of the way
+        self.drive(linear=0.0, angular=0.0)  # stop robot at end
+        self.get_logger().info(f"end drove {distance:.2f}")
 
     def drive(self, linear, angular):
         """
@@ -168,6 +131,53 @@ class PersonFollower(Node):
         msg.linear.x = linear
         msg.angular.z = angular
         self.vel_pub.publish(msg)
+
+    def find_person(self, msg: LaserScan):
+        """
+        Checks the range of laser scans to find the shortest distance, and updates angle and distance of that determined person point
+
+        Args:
+            msg (sensor_msgs.msg.LaserScan)
+        """
+        filtered_ranges = self.handle_scan(msg)
+
+        # calculate angle  of closest point
+        angle = math.radians(min(filtered_ranges, key=filtered_ranges.get))
+
+        min_distance = min(filtered_ranges.values())  # get min distance
+
+        # convert 270 deg+ angle values to negatives, avoid long way spin
+        if angle > (math.pi / 2):
+            angle = angle - (2 * math.pi)  # get neg from angle - full circle
+
+        if min_distance < 1:  # ideally, if we don't update, will "idle"
+            self.get_logger().info(
+                f"found person {min_distance:.2f} m, {math.degrees(angle):.2f} deg away"
+            )
+            self.is_person = True  # enable driving
+            with self.vel_lock:
+                self.turn_angle = angle
+                self.distance = min_distance
+        else:
+            self.is_person = False
+
+    def handle_scan(self, scan: LaserScan):
+        """
+        Handles and processes messages received on scan topic to get the scans corresponding to the front of the robot
+
+        Args:
+            msg (sensor_msgs.msg.LaserScan)
+
+        Returns:
+            A dictionary of the indices, distances narrowed to search area
+        """
+        #  get filtered range corresponding to 90 to -90 clockwise
+        filtered_ranges = []
+        for i, r in enumerate(scan.ranges):
+            if i < 90 or i > 270:
+                filtered_ranges.append((i, r))
+
+        return dict(filtered_ranges)  # convert to dict for ease of use
 
 
 def main(args=None):
